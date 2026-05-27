e85faebe5a0a9f8bbf6116882458501d8f9e8eb4
John Omotani <john.omotani@ukaea.uk>
2026-05-27 10:26:16 +0100

    Extra optimization of BlockDiagonalSolver sparse-matrix ldiv!()

--- src/MPIStaticCondensations.jl
+++ src/MPIStaticCondensations.jl
@@ -1612,20 +1612,22 @@ function ldiv!(x::AbstractSparseMatrixCSC{T},
                     ldiv!(this_x_buffer, s, this_u_buffer)
                     x_flat_start = x_colptr[col]
                     x_flat_end = x_colptr[col+1] - 1
-                    count = x_flat_start
+                    x_col_rowval = @view x_rowval[x_flat_start:x_flat_end]
+                    nxr = x_flat_end - x_flat_start + 1
+                    count = max(searchsortedlast(x_col_rowval, first(bi)) - 1, 1)
                     for (i2, i1) ∈ enumerate(bi)
                         # Assume that the structural non-zero entries of `x` are enough to
                         # contain all the non-zero entries of the solve. Note that the
                         # entries in this_x_buffer that should be structurally zero might
                         # only be zero up to floating-point precision.
-                        while count ≤ x_flat_end && x_rowval[count] < i1
+                        while count ≤ nxr && x_col_rowval[count] < i1
                             count += 1
                         end
-                        if count > x_flat_end
+                        if count > nxr
                             break
                         end
-                        if i1 == x_rowval[count]
-                            x_nzval[count] = this_x_buffer[i2]
+                        if i1 == x_col_rowval[count]
+                            x_nzval[x_flat_start+count-1] = this_x_buffer[i2]
                             count += 1
                         end
                     end
