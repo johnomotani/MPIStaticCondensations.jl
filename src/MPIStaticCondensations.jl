012738bd9dd8388bc53c46a746ff71f212156cd3
John Omotani <john.omotani@ukaea.uk>
2026-07-13 12:17:08 +0100

    continue no MPISchurComplements - extra sync

--- src/MPIStaticCondensations.jl
+++ src/MPIStaticCondensations.jl
@@ -1823,6 +1823,7 @@ function ldiv!(solver::MPIStaticCondensationParallel{T}, U::AbstractVector{T}) w
                         v[i1] += U[i2]
                     end
                 end
+                solver.synchronize_shared()
                 ldiv!(u, v, schur_complement_solver, u, v)
                 for (i1, i2) ∈ zip(partial_local_top_vector_indices, partial_top_sub_range)
                     U[i1] = u[i2]
