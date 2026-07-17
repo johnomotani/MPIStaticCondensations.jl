c4f0d5089cbef4430471f774cd303d9c319173b4
John Omotani <john.omotani@ukaea.uk>
2026-07-17 17:54:38 +0100

    Optimise get_shared_sparse_matrix_csc_buffer()
    ...by caching row values for 'equivalent' columns that have the same set
    of row values.

--- src/shared_sparse_buffers.jl
+++ src/shared_sparse_buffers.jl
@@ -11,7 +11,7 @@ function get_dim_indices!(dimensions, block_sizes, flat_i)
     end
 end
 
-function add_all_row_inds!(rv, idim, dimensions, row_indices, rowind, count, row_count)
+function add_all_row_inds!(rv, idim, dimensions, row_indices, rowind, row_count)
     @inbounds begin
         if idim == 0
             # rowind is constructed as a 0-based index for convenience. Convert to
@@ -25,7 +25,6 @@ function add_all_row_inds!(rv, idim, dimensions, row_indices, rowind, count, row
             end
             if row_count[] ≤ length(row_indices) && rowind == row_indices[row_count[]]
                 push!(rv, row_count[])
-                count[] += 1
                 row_count[] += 1
             end
             return nothing
@@ -33,7 +32,7 @@ function add_all_row_inds!(rv, idim, dimensions, row_indices, rowind, count, row
         d = dimensions[idim]
         rowind *= d.n_local
         for i ∈ 1:d.n_local
-            add_all_row_inds!(rv, idim - 1, dimensions, row_indices, rowind + i - 1, count,
+            add_all_row_inds!(rv, idim - 1, dimensions, row_indices, rowind + i - 1,
                               row_count)
         end
         return nothing
@@ -41,7 +40,7 @@ function add_all_row_inds!(rv, idim, dimensions, row_indices, rowind, count, row
 end
 
 function add_row_inds!(rv, idim, dimensions, block_sizes, nblock_list, row_indices,
-                       block_inds, inner_inds, rowind, count, row_count)
+                       block_inds, inner_inds, rowind, row_count)
     @inbounds begin
         if idim == 0
             # rowind is constructed as a 0-based index for convenience. Convert to
@@ -55,7 +54,6 @@ function add_row_inds!(rv, idim, dimensions, block_sizes, nblock_list, row_indic
             end
             if row_count[] ≤ length(row_indices) && rowind == row_indices[row_count[]]
                 push!(rv, row_count[])
-                count[] += 1
                 row_count[] += 1
             end
             return nothing
@@ -74,7 +72,7 @@ function add_row_inds!(rv, idim, dimensions, block_sizes, nblock_list, row_indic
             for row_inner ∈ 1:block_npoints
                 add_row_inds!(rv, idim - 1, dimensions, block_sizes, nblock_list,
                               row_indices, block_inds, inner_inds,
-                              rowind + row_offset + row_inner - 1, count, row_count)
+                              rowind + row_offset + row_inner - 1, row_count)
             end
         end
         row_offset = (iblock - 1) * block_npoints
@@ -83,7 +81,7 @@ function add_row_inds!(rv, idim, dimensions, block_sizes, nblock_list, row_indic
             # boundaries'.
             block_start = 2
             add_all_row_inds!(rv, idim - 1, dimensions, row_indices, rowind + row_offset,
-                              count, row_count)
+                              row_count)
         else
             block_start = 1
         end
@@ -99,7 +97,7 @@ function add_row_inds!(rv, idim, dimensions, block_sizes, nblock_list, row_indic
             # iel=nelement_local-1, col_igr=ngrid, so only need to add the row_igr=1
             # point.
             add_row_inds!(rv, idim - 1, dimensions, block_sizes, nblock_list, row_indices,
-                          block_inds, inner_inds, rowind + row_offset, count, row_count)
+                          block_inds, inner_inds, rowind + row_offset, row_count)
         else
             for row_inner ∈ block_start:block_npoints+1
                 if row_offset + row_inner > row_end
@@ -109,12 +107,12 @@ function add_row_inds!(rv, idim, dimensions, block_sizes, nblock_list, row_indic
                 end
                 add_row_inds!(rv, idim - 1, dimensions, block_sizes, nblock_list,
                               row_indices, block_inds, inner_inds,
-                              rowind + row_offset + row_inner - 1, count, row_count)
+                              rowind + row_offset + row_inner - 1, row_count)
             end
         end
         if d.dense_boundaries && is_last_point
             add_all_row_inds!(rv, idim - 1, dimensions, row_indices, rowind + dn - 1,
-                              count, row_count)
+                              row_count)
         end
         return nothing
     end
@@ -162,21 +160,115 @@ function get_shared_sparse_matrix_csc_buffer(dimensions::Vector{<:Dimension},
         n_colptr = Ref(-1)
         n_rowval = Ref(-1)
         if shared_comm_rank == 0
-            cp = Int64[]
-            rv = Int64[]
-            count = Ref(1)
-            row_count = Ref(1)
-
-            for col ∈ column_indices
-                push!(cp, count[])
-                block_inds, inner_inds = get_dim_indices!(dimensions, block_sizes,
-                                                          col - 1)
-                row_count[] = 1
-                add_row_inds!(rv, length(dimensions), dimensions, block_sizes,
-                              nblock_list, row_indices, block_inds, inner_inds, 0, count,
-                              row_count)
+            # Columns can be 'equivalent' in the sense that they have exactly the same
+            # non-empty row indices. In a single dimension, two points are equivalent if
+            # they are in the interior of the same element, or if they are the point (in
+            # case they are boundary points). Columns are equivalent if their positions in
+            # every dimension are equivalent.
+            cartinds_list = CartesianIndices(Tuple(d.n_local for d ∈ dimensions))
+            function get_equivalents_lookup(d, nblock, block_size)
+                eq_list = zeros(ind_type, d.n_local)
+                eq = ind_type(1)
+                count = ind_type(0)
+                interior_range = 2:block_size*(d.ngrid-1)
+                d_n_local = d.n_local
+                if d.remove_boundaries
+                    for _ ∈ 1:nblock
+                        # Lower boundary
+                        eq_list[count+=1] = eq
+                        eq += 1
+                        if count ≥ d_n_local - 1
+                            # Last block may not be full size, so make sure not to go
+                            # outside the bounds of the dimension.
+                            break
+                        end
+                        for _ ∈ interior_range
+                            eq_list[count+=1] = eq
+                            if count == d_n_local - 1
+                                # Last block may not be full size, so make sure not to go
+                                # outside the bounds of the dimension.
+                                break
+                            end
+                        end
+                        eq += 1
+                    end
+                    # Upper boundary
+                    if count == d_n_local - 1
+                        eq_list[count+=1] = eq
+                    end
+                else
+                    for _ ∈ 1:block_size*(d.ngrid-1)
+                        eq_list[count+=1] = eq
+                    end
+                    eq += 1
+                    for _ ∈ 2:nblock
+                        # Lower boundary
+                        eq_list[count+=1] = eq
+                        eq += 1
+                        if count ≥ d_n_local - 1
+                            # Last block may not be full size, so make sure not to go
+                            # outside the bounds of the dimension.
+                            break
+                        end
+                        for _ ∈ interior_range
+                            eq_list[count+=1] = eq
+                            if count == d_n_local - 1
+                                # Last block may not be full size, so make sure not to go
+                                # outside the bounds of the dimension.
+                                break
+                            end
+                        end
+                        eq += 1
+                    end
+                    # Upper boundary
+                    if count == d_n_local - 1
+                        eq_list[count+=1] = eq - 1
+                    end
+                end
+                return eq_list
             end
-            push!(cp, count[])
+            equivalents_lookup_lists = [get_equivalents_lookup(d, nb, bs)
+                                        for (d, nb, bs)
+                                        ∈ zip(dimensions, nblock_list, block_sizes)]
+            n_equivalents_list = [eq[end] for eq ∈ equivalents_lookup_lists]
+            function equivalence_ind(flat_i)
+                cartind = Tuple(cartinds_list[flat_i])
+                eq_ind = ind_type(0)
+                for (i, neq, eq_lookup) ∈ zip(reverse(cartind),
+                                              reverse(n_equivalents_list),
+                                              reverse(equivalents_lookup_lists))
+                    eq_ind *= neq
+                    eq_ind += eq_lookup[i] - 1
+                end
+                # eq_ind was constructed as 0-based index. Convert back to 1-based.
+                eq_ind += 1
+                return eq_ind
+            end
+
+            cp = ind_type[]
+            rv = ind_type[]
+            row_count = Ref(1)
+            rv_lookup = Dict{ind_type,Vector{ind_type}}()
+            for col ∈ column_indices
+                push!(cp, length(rv) + 1)
+                col_eq = equivalence_ind(col)
+                if col_eq ∈ keys(rv_lookup)
+                    col_rv = rv_lookup[col_eq]
+                else
+                    col_rv = ind_type[]
+                    block_inds, inner_inds = get_dim_indices!(dimensions, block_sizes,
+                                                              col - 1)
+                    row_count[] = 1
+                    add_row_inds!(col_rv, length(dimensions), dimensions, block_sizes,
+                                  nblock_list, row_indices, block_inds, inner_inds, 0,
+                                  row_count)
+                    rv_lookup[col_eq] = col_rv
+                end
+                new_n = length(col_rv)
+                resize!(rv, length(rv) + new_n)
+                rv[end-new_n+1:end] .= col_rv
+            end
+            push!(cp, length(rv) + 1)
 
             n_colptr[] = length(cp)
             n_rowval[] = length(rv)
