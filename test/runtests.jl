c06d59123bc50d14c9e1fb9fa955e9e63dbfc5e5
John Omotani <john.omotani@ukaea.uk>
2026-08-13 19:05:15 +0100

    Use failfast=true for tests
    Otherwise the output log can get excessively large on CI servers when
    many tests fail.

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ include("test_finite_element_matrices.jl")
 include("test_multivariable_finite_element_matrices.jl")
 
 function runtests()
-    @testset "MPIStaticCondensations.jl" begin
+    @testset "MPIStaticCondensations.jl" failfast=true begin
         test_indices()
         test_finite_element_matrices()
         test_multivariable_finite_element_matrices()
